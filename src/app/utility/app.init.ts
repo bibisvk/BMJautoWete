c775ee1a34a5341b78cf7973f1aeed77c4fac0d1
mfulajtar <mfulajtar@umb.sk>
2022-05-21 15:53:53 +0200

    ide keycloak

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -1,16 +1,16 @@
 import {KeycloakService} from 'keycloak-angular';
 
-export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
+export function initializeKeycloak(keycloak: KeycloakService){
   return () =>
     keycloak.init({
       config: {
-        url: 'http://localhost:8090/auth',
+        url: 'http://localhost:8090',
         realm: 'BMJ',
         clientId: 'BMJclient'
       },
       initOptions: {
-        checkLoginIframe: true,
-        checkLoginIframeInterval: 25
+        onLoad: "login-required",
+        flow: "standard"
       }
     });
 }
